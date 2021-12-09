9ac8ae58c719aac1dd2e9812b41def5d059b253d
Simon Groom <simon.groom@ascentcloud.io>
2021-12-09 16:15:19 -0500

    implement codepipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,13 +1,29 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Repository } from "aws-cdk-lib/aws-codecommit";
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class WorkshipPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Repository(this, 'WorkshopRepo', {
+    const repo = new Repository(this, 'WorkshopRepo', {
       repositoryName: "WorkshopRepo",
+    });
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'WorkshopPipeline',
+      synth: new CodeBuildStep('SynthStep', {
+        input: CodePipelineSource.codeCommit(repo, 'main'),
+        installCommands: [
+          'npm install -g aws-cdk'
+        ],
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
     });
   }
 }
